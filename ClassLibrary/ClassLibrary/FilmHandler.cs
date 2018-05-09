6a646e86cbcbd04c5966086290174520c487f547
andrei <andreichayeuski@gmail.com>
2018-05-09 10:19:22 +0300

    class library

---
+++ ClassLibrary/ClassLibrary/FilmHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization;
+namespace ClassLibrary
+{
+    public class FilmHandler
+    {
+        public static byte[] ConvertFilmListToByteArray(List<Film> list)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(stream, list);
+            return stream.ToArray();
+        }
+
+        public static List<Film> ConvertByteArrayToFilmList(byte[] bytearray)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryWriter binaryFormatter = new BinaryWriter(stream);
+            List<Film> result = new List<Film>();
+            stream.Write(bytearray, 0, bytearray.Length);
+            stream.Seek(0, SeekOrigin.Begin);
+            BinaryFormatter bf = new BinaryFormatter();
+            result = (List<Film>)bf.Deserialize(stream);
+            return result;
+        }
+    }
+}
